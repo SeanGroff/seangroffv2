97ad002bae4aca3541945f5ec96857079b85c7f6
Sean <sean@codekoalas.com>
2017-05-26 17:01:20 -0500

    Quick margin fix for mobile first design

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import NavBar from '../components/NavBar';
 import hero from '../assets/hero1.jpg';
 import Colors from '../utils/Colors';
+import MediaQueries from '../utils/MediaQueries';
+
+const { mediumUp } = MediaQueries;
 
 const { transparentDarkBlue } = Colors;
 
@@ -20,11 +23,16 @@ const HeroImage = styled.header`
 const Me = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
   height: 100%;
-  margin-top: -100px;
+  margin-top: 20px;
+
+  ${mediumUp} {
+    justify-content: center;
+    margin-top: -100px
+  }
 `;
 
 const Hero = props => {
