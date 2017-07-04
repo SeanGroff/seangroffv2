e3ae4ae629b3f7f438b7eb39fb0e68b5730d00bb
Sean <sean@codekoalas.com>
2017-07-03 23:47:43 -0500

    Added all projects to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,10 +5,15 @@ import Heading from './Heading';
 import Colors from '../utils/Colors';
 import MediaQueries from '../utils/MediaQueries';
 import fccLeaderboardImg from '../assets/fcc-leaderboard.jpg';
+import fccMarkdownImg from '../assets/fcc-markdown.jpg';
+import fccWikiImg from '../assets/wiki-viewer.jpg';
+import fccPomodoroImg from '../assets/pomodoro.jpg';
+import eggheadImg from '../assets/egghead.jpg';
+import fpImg from '../assets/fp.jpg';
 import '../fonts/OpenSansBold.css';
 import '../fonts/LoraRegular.css';
 
-const { bgColorDark, bgColorLight, lightText, transparentDarkBlue } = Colors;
+const { bgColorDark, lightText, transparentDarkBlue } = Colors;
 const { smallUp, mediumUp, largeUp } = MediaQueries;
 
 const projects = [
@@ -19,34 +24,34 @@ const projects = [
     image: fccLeaderboardImg,
   },
   {
-    title: 'project2',
-    description: 'this app is the coolest app ever. It is great!',
-    href: 'https://www.google.com',
-    image: 'https://placebear.com/300/200',
+    title: 'FCC Markdown Previewer',
+    description: 'Enter markdown code on the left to see a live preview of your markdown code on the right. Built with React',
+    href: 'https://github.com/SeanGroff/markdown-previewer',
+    image: fccMarkdownImg,
   },
   {
-    title: 'project3',
-    description: 'this app is the coolest app ever. It is great!',
-    href: 'https://www.google.com',
-    image: 'https://placebear.com/300/200',
+    title: 'FCC Wikipedia Viewer',
+    description: 'Built with RxJS, enter a search term and receive query results from the wikipedia API',
+    href: 'https://codepen.io/sgroff04/pen/dWzZjw',
+    image: fccWikiImg,
   },
   {
-    title: 'project4',
-    description: 'this app is the coolest app ever. It is great!',
-    href: 'https://www.google.com',
-    image: 'https://placebear.com/300/200',
+    title: 'FCC Pomodoro Timer',
+    description: 'Built with jQuery, take effecient breaks using the Pomodoro Technique',
+    href: 'https://codepen.io/sgroff04/pen/gaPKBd',
+    image: fccPomodoroImg,
   },
   {
-    title: 'project5',
-    description: 'this app is the coolest app ever. It is great!',
-    href: 'https://www.google.com',
-    image: 'https://placebear.com/300/200',
+    title: 'Egghead a Day',
+    description: 'Watch an egghead.io video a day and provide a code example to teach others',
+    href: 'https://github.com/SeanGroff/eggheadaday',
+    image: eggheadImg,
   },
   {
-    title: 'project6',
-    description: 'this app is the coolest app ever. It is great!',
-    href: 'https://www.google.com',
-    image: 'https://placebear.com/300/200',
+    title: 'JS FP Functions from Scratch',
+    description: 'Popular native JavaScript, lodash, and RxJS Array functions built from scratch for my personal educational purposes',
+    href: 'https://github.com/SeanGroff/functions-from-scratch',
+    image: fpImg,
   },
 ];
 
@@ -105,18 +110,22 @@ const Title = styled.h2`
 `;
 
 const Description = styled.p`
-  font-size: 14px;
+  font-size: 12px;
   font-family: Lora-Regular;
-  line-height: 20px;
   opacity: 0;
   transform: translateY(100px);
   transition: all 0.3s ease-in-out;
+  line-height: 20px;
+
+  ${mediumUp} {
+    font-size: 14px;
+  }
 `;
 
 const Overlay = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   position: absolute;
   top: 0;
@@ -154,11 +163,12 @@ const Project = styled.a`
   height: 200px;
   position: relative;
   overflow: hidden;
-  border: 3px solid ${bgColorLight};
+  border: 3px solid #3498DB;
 ;
 
   &:hover {
     cursor: pointer;
+    border: 3px solid #E74C3C;
 
     > ${Image} {
       transform: scale(1.1);
