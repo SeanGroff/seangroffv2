70ec72402491a5c67910903b3552fa8e287017c1
Sean <sgroff04@gmail.com>
2017-12-26 17:42:44 -0600

    Fixes #2

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -1,9 +1,9 @@
-import fccLeaderboardImg from '../assets/fcc-leaderboard.jpg';
-import fccMarkdownImg from '../assets/fcc-markdown.jpg';
-import fccWikiImg from '../assets/wiki-viewer.jpg';
-import fccPomodoroImg from '../assets/pomodoro.jpg';
-import eggheadImg from '../assets/egghead.jpg';
-import fpImg from '../assets/fp.jpg';
+import fccLeaderboardImg from '../assets/fcc-leaderboard.png';
+import fccMarkdownImg from '../assets/fcc-markdown.png';
+import fccWikiImg from '../assets/wiki-viewer.png';
+import fccPomodoroImg from '../assets/pomodoro.png';
+import eggheadImg from '../assets/egghead.png';
+import fpImg from '../assets/fp.png';
 
 export default [
   {
@@ -14,31 +14,36 @@ export default [
   },
   {
     title: 'FCC Markdown Previewer',
-    description: 'Enter markdown code on the left to see a live preview of your markdown code on the right. Built with React',
+    description:
+      'Enter markdown code on the left to see a live preview of your markdown code on the right. Built with React',
     href: 'https://github.com/SeanGroff/markdown-previewer',
     image: fccMarkdownImg,
   },
   {
     title: 'FCC Wikipedia Viewer',
-    description: 'Built with RxJS, enter a search term and receive query results from the wikipedia API',
+    description:
+      'Built with RxJS, enter a search term and receive query results from the wikipedia API',
     href: 'https://codepen.io/sgroff04/pen/dWzZjw',
     image: fccWikiImg,
   },
   {
     title: 'FCC Pomodoro Timer',
-    description: 'Built with jQuery, take effecient breaks using the Pomodoro Technique',
+    description:
+      'Built with jQuery, take effecient breaks using the Pomodoro Technique',
     href: 'https://codepen.io/sgroff04/pen/gaPKBd',
     image: fccPomodoroImg,
   },
   {
     title: 'Egghead a Day',
-    description: 'Watch an egghead.io video a day and provide a code example to teach others',
+    description:
+      'Watch an egghead.io video a day and provide a code example to teach others',
     href: 'https://github.com/SeanGroff/eggheadaday',
     image: eggheadImg,
   },
   {
     title: 'JS FP Functions from Scratch',
-    description: 'Popular native JavaScript, lodash, and RxJS Array functions built from scratch for my personal educational purposes',
+    description:
+      'Popular native JavaScript, lodash, and RxJS Array functions built from scratch for my personal educational purposes',
     href: 'https://github.com/SeanGroff/functions-from-scratch',
     image: fpImg,
   },
